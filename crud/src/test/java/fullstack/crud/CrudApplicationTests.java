5e79b674129574f94ac3f44d0300f2796f97e87e
Kauan Bertalha <kauan.hb2004@gmail.com>
2025-09-13 20:16:48 -0300

    init

---
+++ crud/src/test/java/fullstack/crud/CrudApplicationTests.java
@@ -0,0 +1,13 @@
+package fullstack.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
